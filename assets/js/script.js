e45697e5f99d0ba3e053ff7b4dc980d0418cda0c
Renan Soares <renansoaresffc@gmail.com>
2022-01-18 16:52:38 -0300

    typed.js and hover img card

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Typed.js
+var typed = new Typed('.animation', {
+    strings: ["O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."],
+    typeSpeed: 45,
+});
+
